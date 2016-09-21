606486ecaf98da16cd29e97c80cf86c5eb2fe313
colin <colin1124x@gmail.com>
2016-09-21 16:42:47 +0800

    init

---
+++ server/room.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"log"
+	"sync"
+
+	"golang.org/x/net/websocket"
+)
+
+// Chatroom handle all rooms
+type Chatroom interface {
+	Checkin(string, string, *websocket.Conn) Room
+	GC(*websocket.Conn)
+}
+
+type chatroom struct {
+	*sync.Mutex
+	rooms map[string]Room
+}
+
+// NewChatroom return a chatroom instance
+func NewChatroom() Chatroom {
+	return &chatroom{
+		Mutex: &sync.Mutex{},
+		rooms: make(map[string]Room),
+	}
+}
+
+func (cm *chatroom) Checkin(chatName, userName string, c *websocket.Conn) Room {
+	cm.Lock()
+	defer cm.Unlock()
+
+	r, ok := cm.rooms[chatName]
+	if !ok {
+		r = NewRoom()
+		cm.rooms[chatName] = r
+	}
+
+	r.Join(userName, c)
+
+	return r
+}
+
+func (cm *chatroom) GC(c *websocket.Conn) {
+
+	cm.Lock()
+	defer cm.Unlock()
+	for name, r := range cm.rooms {
+		if r.Has(c) {
+			if 0 >= r.Leave(c) {
+				delete(cm.rooms, name)
+				return
+			}
+		}
+	}
+}
+
+// Room is used for put conn of ws
+type Room interface {
+	Members() int
+	Has(*websocket.Conn) bool
+	Join(string, *websocket.Conn) int
+	Leave(*websocket.Conn) int
+	Broadcast(interface{})
+}
+
+type room struct {
+	*sync.RWMutex
+	seats map[*websocket.Conn]string
+}
+
+// NewRoom return a room instance
+func NewRoom() Room {
+	return &room{
+		RWMutex: &sync.RWMutex{},
+		seats:   make(map[*websocket.Conn]string),
+	}
+}
+
+func (r *room) Members() int {
+	r.RLock()
+	defer r.RUnlock()
+
+	return len(r.seats)
+}
+
+func (r *room) Has(c *websocket.Conn) bool {
+	r.RLock()
+	defer r.RUnlock()
+
+	_, ok := r.seats[c]
+	return ok
+}
+
+func (r *room) Join(name string, c *websocket.Conn) int {
+	r.Lock()
+	defer r.Unlock()
+	r.seats[c] = name
+
+	return len(r.seats)
+}
+
+func (r *room) Leave(c *websocket.Conn) int {
+	r.Lock()
+	defer r.Unlock()
+	delete(r.seats, c)
+
+	return len(r.seats)
+}
+
+func (r *room) Broadcast(msg interface{}) {
+	r.RLock()
+	defer r.RUnlock()
+
+	for c, name := range r.seats {
+		if err := websocket.JSON.Send(c, msg); err != nil {
+			log.Printf("[broadcast] fail on %s\n", name)
+		}
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/net/websocket"
+)
+
+var env = struct {
+	wsAddr     string
+	clientAddr string
+	clientDir  string
+}{}
+
+func main() {
+
+	flag.StringVar(&env.wsAddr, "ws", ":9090", "WebSocket serve port")
+	flag.StringVar(&env.clientAddr, "c", ":9000", "Chatroom UI serve port")
+	flag.StringVar(&env.clientDir, "ui", "client", "Chatroom UI dir")
+	flag.Parse()
+
+	go func() {
+
+		cm := NewChatroom()
+
+		err := http.ListenAndServe(env.wsAddr, websocket.Handler(func(c *websocket.Conn) {
+			roomName := c.Request().URL.Path
+			connName := c.Request().URL.Query().Get("name")
+
+			log.Printf("[%s] %s joined\n", roomName, connName)
+
+			// 取得聊天室
+			r := cm.Checkin(roomName, connName, c)
+
+			// 讀取資料
+			for {
+				var s string
+				err := websocket.Message.Receive(c, &s)
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					log.Println("[ws receive]", err)
+				} else {
+					log.Printf("[%s] %s: %s\n", roomName, connName, s)
+					r.Broadcast(struct {
+						Name string `json:"name"`
+						Msg  string `json:"msg"`
+					}{connName, s})
+				}
+			}
+			log.Printf("[%s] %s leaved\n", roomName, connName)
+
+		}))
+		log.Println("[ws] shutdown:", err)
+	}()
+
+	go func() {
+		err := http.ListenAndServe(env.clientAddr, http.FileServer(http.Dir(env.clientDir)))
+		log.Println("[ui] shutdown:", err)
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGTERM, syscall.SIGINT)
+	sign := <-shutdown
+	log.Printf("[shutdown] %s\n", sign)
+}
